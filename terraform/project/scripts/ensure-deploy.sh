2e3f145ca71389b04437d13ad18d1555dd028c6b
Chris Dickinson <christopher.s.dickinson@gmail.com>
2020-03-09 00:12:39 -0700

    feat: keep github actions up to date

--- terraform/project/scripts/ensure-deploy.sh
+++ terraform/project/scripts/ensure-deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 set -e
-
+exec 6>&1
+exec > /dev/null
+exec 2>&1
 eval "$(jq -r '@sh "URL=\(.url) DEPLOY=\(.deploy)"')"
 dir=$(mktemp -d)
 
@@ -11,6 +13,15 @@ for i in $DEPLOY/*.yml; do
   cp $i $dir/.github/workflows
 done
 cd $dir
-git add .github/workflows
-git commit -m 'update github deploy action'
-git push
+result=0
+git add .github/workflows || result=$?
+git commit -m 'update github deploy action' || result=$?
+if [ $result -ne 0 ]; then
+  exec 1>&6 6>&-
+  echo '{"result": "no updates for '$URL'"}'
+else
+  git push
+  exec 1>&6 6>&-
+  echo '{"result": "updated '$URL'"}'
+fi
+rm -rf $dir
